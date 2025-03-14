f1230aa555f458a5b4d69665acf8b80b09740de9
Pikott143 <Theunseemaiden@gmail.com>
2025-03-14 17:36:14 +0800

    Updates

--- assets/md.transition-legacy-DhPJGcQv.js
+++ assets/md.transition-legacy-DhPJGcQv.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D_EL9spo.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-7BJjfiHc.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
