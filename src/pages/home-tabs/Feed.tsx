9ebcb60be85c6733921397626beaa699904e861a
Pikott143 <Theunseemaiden@gmail.com>
2025-04-21 22:14:35 +0800

    added

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -11,7 +11,12 @@ import {
   IonPage,
   IonSearchbar,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent
 } from '@ionic/react';
 import { add, colorPalette, document, globe } from 'ionicons/icons';
 
@@ -30,23 +35,37 @@ const Feed: React.FC = () => {
           <IonTitle>Feed</IonTitle>
         </IonToolbar>
         <IonSearchbar
-          placeholder="Search Favorites..."
+          placeholder="Search Feed..."
           onIonInput={handleSearch}
-          debounce={0}
+          debounce={500} // optional debounce
         />
       </IonHeader>
 
-      <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Feed
-        </div>
+      <IonContent fullscreen className="ion-padding">
+        {/* Sample Card */}
+        <IonCard>
+          <img
+            alt="One Piece crew in Egghead outfits"
+            src="public3c6483bd.webp"
+          />
+          
+          <IonCardHeader>
+            <IonCardTitle>Card Title</IonCardTitle>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            Here's a small text description for the card content. Nothing more, nothing less.
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+      <img alt="SL" src="https://scontent.fcgm1-1.fna.fbcdn.net/v/t39.30808-6/290905173_10159776384714340_4225162534160083466_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=0b6b33&_nc_eui2=AeHMkf5donuu5kd8vigizemV0Mvpd8XxU0TQy-l3xfFTRHB9hblUjnbe1H0p21YV_PQxiuI1NhfRl185iHgVnqo3&_nc_ohc=Mvgc7AfcywgQ7kNvgGTQwoR&_nc_oc=AdhSK19rvDIsYfp_acr_yxHYGADcnbwzgeOR03yNiySF65lVo2sZ6XAhMigBQj97aKA&_nc_zt=23&_nc_ht=scontent.fcgm1-1.fna&_nc_gid=A8SFDDUmZm6bvtzOD145847&oh=00_AYAHyg9E4sbA14Pa943ccII-tSsKS2fCcHsO1B4ZDMQPiw&oe=67CA9026" />
+      <IonCardHeader>
+        <IonCardTitle>Solo Leveling</IonCardTitle>
+      </IonCardHeader>
+      <IonCardContent>The worldwide dungeon-crawling phenomenon is getting an anime adaptation!</IonCardContent>
+    </IonCard>
 
         {/* Floating Action Button */}
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
@@ -71,3 +90,4 @@ const Feed: React.FC = () => {
 };
 
 export default Feed;
+
--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -14,25 +14,33 @@ const Search: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        {/* First Toolbar with title and menu button */}
         <IonToolbar>
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
           <IonTitle>Search</IonTitle>
         </IonToolbar>
-
-        {/* Second Toolbar with Searchbar */}
-        <IonToolbar>
-          <IonSearchbar placeholder="Search something..." />
-        </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen className="ion-padding">
-        {/* Content here */}
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
+          <IonSearchbar
+            placeholder="Search something..."
+            style={{ width: '90%', maxWidth: '500px' }}
+          />
+        </div>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Search;
+
