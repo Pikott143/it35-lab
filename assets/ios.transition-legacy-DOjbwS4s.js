2526dfbbfa8c043d01ac7dd885e6504f9635d73a
Pikott143 <Theunseemaiden@gmail.com>
2025-04-23 10:53:33 +0800

    Updates

--- assets/ios.transition-legacy-DOjbwS4s.js
+++ assets/ios.transition-legacy-DOjbwS4s.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3U91Cu8g.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-D6k1aii7.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-B206j15a.js
+++ assets/md.transition-legacy-B206j15a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3U91Cu8g.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-D6k1aii7.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
