df7ac132ada991234b2d9ddcdb51256ccacbc197
Pikott143 <Theunseemaiden@gmail.com>
2025-04-23 08:51:13 +0800

    Updates

--- assets/ios.transition-legacy-DuaCv8NW.js
+++ assets/ios.transition-legacy-DuaCv8NW.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-7BJjfiHc.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-3U91Cu8g.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-Cy6EYkwd.js
+++ assets/keyboard2-legacy-Cy6EYkwd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-7BJjfiHc.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-3U91Cu8g.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
