25acbcf94b6b961cb6575cf7d4a9193c607f768f
Pikott143 <Theunseemaiden@gmail.com>
2025-04-23 12:20:23 +0800

    Updates

--- assets/md.transition-legacy-4DivNva7.js
+++ assets/md.transition-legacy-4DivNva7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D6k1aii7.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-D4a_K8TE.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
