920b9ac173893807451791b1cbaa9b46de608245
Pikott143 <Theunseemaiden@gmail.com>
2025-04-25 01:07:20 +0800

    Updates

--- assets/md.transition-legacy-DMkuwzvw.js
+++ assets/md.transition-legacy-DMkuwzvw.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D4a_K8TE.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-B3KBoWUs.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-4C4Jf0J1.js
+++ assets/swipe-back-legacy-4C4Jf0J1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D4a_K8TE.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-B3KBoWUs.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
