7b83e2902a57789bc3e3c4f151d9b0d67cedcea3
Pikott143 <Theunseemaiden@gmail.com>
2025-04-28 03:38:56 +0800

    Updates

--- assets/swipe-back-legacy-CD9K0-Ch.js
+++ assets/swipe-back-legacy-CD9K0-Ch.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BPibZiFn.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CSCo_cPT.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
